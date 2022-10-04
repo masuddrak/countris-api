a0e6d8ffaad5302a6bba6cfbe6d784013aefd0e7
masud rana <masudranaabc@gmail.com>
2022-10-05 01:24:40 +0600

    added countris api data loaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Countris></Countris>
     </div>
   );
 }
+function Countris() {
+  const [cunris, setCountris] = useState([])
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+      .then(res => res.json())
+      .then(data => setCountris(data))
+  }, [])
+
+  return (
+    <div className="contris">
+      <h2>All Countris {cunris.length}</h2>
+      <hr style={{ "width": "40%", "color": "red" }}></hr>
+      {
+        cunris.map(country => <Country
+          name={country.name.common}
+          country={country}
+          key={country.capital}
+        ></Country>)
+      }
+    </div>
+  )
+}
+function Country(props) {
+  console.log(props.country)
+  return (
+    <div className="country">
+      <h2>{props.name}</h2>
+      <img src={props.country.flags.png} alt="" />
+    </div>
+  )
+}
+
 
 export default App;
